2d188a1aca4390f52ba00a54e70a350712579d7e
huangxiaomin <huangxiaomin@sensetime.com>
2024-11-19 15:23:15 +0800

    feat: 主页样式修改

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,59 @@
+'use client'
+import { useRouter } from 'next/navigation'
+import './globals.css'
 
 export default function Home() {
+  const router = useRouter()
+  const data = [
+    {
+      title: 'useContext、useReducer', 
+      url: '/home',
+      desc: '等同于vue中的provide、inject'
+    },
+    {
+      title: '待定', 
+      url: '/',
+      desc: ''
+    }
+  ]
+  const goDetail = (params: {title: string, url: string, desc: string}) => {
+    router.push(params.url)
+  }
+  // 生成随机颜色 
+  const randomColor = () => {
+    function isLightColor(color: string) {
+      // 判断颜色是否为浅色
+      const rgb = parseInt(color.substring(1), 16);
+      const r = (rgb >> 16) & 0xff;
+      const g = (rgb >> 8) & 0xff;
+      const b = (rgb >> 0) & 0xff;
+      const luma = 0.2126 * r + 0.7152 * g + 0.0722 * b;
+      return luma > 170; // 调整阈值以适应你定义的浅色范围
+    }
+    // 生成浅色系随机颜色
+    let color = '#';
+    while(!isLightColor(color)){
+      color = '#' + Math.floor(Math.random()*16777215).toString(16);
+    }
+    return color;
+  }
   return (
-    <div>
+    <div className="content" suppressHydrationWarning>
+      {
+        data.map((item, index) => {
+          return (
+          <div 
+            key={index} 
+            style={{'background': randomColor()}} 
+            className="card" 
+            onClick={() => goDetail(item)} 
+            suppressHydrationWarning
+          >
+            <div className="title">{item.title}</div>
+            <div className="grey">{item.desc}</div>
+          </div>
+        )})
+      }
     </div>
   );
 }
